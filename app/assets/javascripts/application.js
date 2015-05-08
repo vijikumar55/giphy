2640559ed8125801abdacdfe3b957184bcda1b3c
Viji Kumar <vijikumar@carmatec.com>
2015-05-08 16:14:24 +0530

    for giphy application

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+
+$(document).ready(function(){
+$('button').click(function() {
+		var search_string = $('#search').val()
+		$.ajax({
+			url : "/search",
+			data : {
+				value : search_string
+			},
+			type : 'get',
+			success : function(result) {
+				$("#gifs_view").html(result)
+			}
+		});
+	}); 
+});
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  
+  def index
+    ## caching trending gif response. It will increase application performance.
+    Rails.cache.fetch('@trending',{expires_in: 1.hours}) do
+      ##HTTParty is a gem, which is used to communicate with API
+      response = HTTParty.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=9")
+      ## I am collecting only the images,as we are going to show only GIF's
+      @trending = response.parsed_response["data"].collect{|gif| gif["images"]["fixed_height"]["url"]}
+    end
+  end
+
+  def search
+    ## i am converting the string, which entered in the UI.
+    search = params["value"].split(" ").join("+")
+    search_response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=#{search}&api_key=dc6zaTOxFJmzC&limit=9")
+    @search_results=search_response.parsed_response["data"].collect{|gif| gif["images"]["fixed_height"]["url"]}
+    render :layout=>false
+  end
+
+end
