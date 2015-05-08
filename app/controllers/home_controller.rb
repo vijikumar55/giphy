de212e4fb9bda029415e70031db84091ac56f350
Viji Kumar <vijikumar@carmatec.com>
2015-05-08 16:39:21 +0530

    for validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,14 @@ class HomeController < ApplicationController
   
   def index
     ## caching trending gif response. It will increase application performance.
+    Rails.cache.delete('@trending') if @trending.nil?
     Rails.cache.fetch('@trending',{expires_in: 1.hours}) do
       ##HTTParty is a gem, which is used to communicate with API
       response = HTTParty.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=9")
       ## I am collecting only the images,as we are going to show only GIF's
       @trending = response.parsed_response["data"].collect{|gif| gif["images"]["fixed_height"]["url"]}
     end
+    
   end
 
   def search
