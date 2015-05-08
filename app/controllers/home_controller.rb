838d4cbcba77d45f273a8fceabc7271a7d6e64b9
Viji Kumar <vijikumar@carmatec.com>
2015-05-08 17:21:18 +0530

    Moved key and api url to environment variables for security purpose

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     Rails.cache.delete('@trending') if @trending.nil?
     Rails.cache.fetch('@trending',{expires_in: 1.hours}) do
       ##HTTParty is a gem, which is used to communicate with API
-      response = HTTParty.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=9")
+      response = HTTParty.get("#{ENV['API_URL']}/gifs/trending?api_key=#{ENV['KEY']}&limit=9")
       ## I am collecting only the images,as we are going to show only GIF's
       @trending = response.parsed_response["data"].collect{|gif| gif["images"]["fixed_height"]["url"]}
     end
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
   def search
     ## i am converting the string, which entered in the UI.
     search = params["value"].split(" ").join("+")
-    search_response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=#{search}&api_key=dc6zaTOxFJmzC&limit=9")
+    search_response = HTTParty.get("#{ENV['API_URL']}/gifs/search?q=#{search}&api_key=#{ENV['KEY']}&limit=9")
     @search_results=search_response.parsed_response["data"].collect{|gif| gif["images"]["fixed_height"]["url"]}
     render :layout=>false
   end
